6eed914d003dfe275a40cf44e4a5971c9d3a32d2
haleyouellette0810 <36276282+haleyouellette0810@users.noreply.github.com>
2018-04-04 11:59:08 -0700

    got the grid to work:

--- sketches/gol.js
+++ sketches/gol.js
@@ -1,29 +1,43 @@
 var grid;
+// var width = 400;
+// var height = 400; 
 
-function setup () {
+
+function setup() {
   createCanvas(400, 400);
-  // grid = new Grid(20);
+  grid = new Grid(20);
 }
 
-function draw () {
+function draw() {
   background(250);
-  
-  // grid.draw();
+grid.draw();
 }
 
 class Grid {
-  constructor () {
-    // update the contructor to take cellSize as a parameter
-    // use cellSize to calculate and assign values for numberOfColumns and numberOfRows
+  constructor(cellSize) {
+    this.cellSize = cellSize;
+    this.numberOfColumns = height / this.cellSize;
+    this.numberOfRows = width / this.cellSize;
   }
 
-  draw () {
-    for (var column = 0; column < this.numberOfColumns; column ++) {
+  draw(){
+    for (var column = 0; column < this.numberOfColumns; column++) {
       for (var row = 0; row < this.numberOfRows; row++) {
-        fill(240); 
+        fill(240);
         noStroke();
         rect(column * this.cellSize + 1, row * this.cellSize + 1, this.cellSize - 1, this.cellSize - 1);
       }
     }
   }
+
+
+  
 }
+
+
+
+ 
+
+
+
+
